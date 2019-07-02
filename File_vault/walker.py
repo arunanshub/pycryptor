60ab7803eac3ab93b5f03bac0c74c37abc9192e6
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-07-02 16:47:30 +0530

    Update walker.py

--- File_vault/walker.py
+++ File_vault/walker.py
@@ -1,24 +1,29 @@
 import os
 
-def walker(filepath):
-  
-  """
-  This function recursively walks into a specified
-  directory *filepath* and yeilds (i.e., creates generator
-  object) which specifies the absolute path of the files
-  The commented out part is for filtering out files with only
-  targeted extensions.
-  """
-  
-  path0 = os.path.normpath(filepath)
-  path = os.path.abspath(path0)
-  
-  try:
-    for (root, dirs, files) in os.walk(path):
-      for file in files:
-      # extn = os.path.splitext(file)[1]
-      # if extn in extns
-        target = os.path.join(root, file)
-        yield target
-  except Exception:
-    pass
+
+# define an extension list here if you want to filter
+# out only selected extensions
+
+def walker(file_path):
+    """
+    This function recursively walks into a specified
+    directory *file_path* and yields (i.e., creates generator
+    object) which specifies the absolute path of the files
+    The commented out part is for filtering out files with only
+    targeted extensions.
+    """
+
+    path0 = os.path.normpath(file_path)
+    path = os.path.abspath(path0)
+
+    try:
+        for (root, dirs, files) in os.walk(path):
+            for file in files:
+                # extn = os.path.splitext(file)[1]
+                # if extn in extension_list
+
+                # Indent the two lines if you want to filter out
+                target = os.path.join(root, file)
+                yield target
+    except Exception:
+        pass
