33d0ae1ad1ed0501ef3fdfb206c031346b86790a
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-08-04 13:09:18 +0530

    Update Locker.py

--- cryptography_locker/Locker.py
+++ cryptography_locker/Locker.py
@@ -15,8 +15,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -111,7 +111,7 @@ def locker(file_path, password, remove=True, **kwargs):
                  encrypted or decrypted will be removed.
                  (Default: True).
     """
-    
+
     if kwargs:
         block_size = kwargs.get('block_size', 64 * 1024)
         ext = kwargs.get('ext', '.0DAY').strip(string.whitespace)
