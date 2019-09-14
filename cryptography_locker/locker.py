c5ccf732c2260a6eb54ed98f6ae1de56d1ebdca2
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-09-14 14:24:34 +0530

    Update locker.py

--- cryptography_locker/locker.py
+++ cryptography_locker/locker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Locker v4.0 (follows new protocol)
-# Implemented as class
+# Locker v4.1 (follows new protocol)
+# Implemented as function
 #
 # =============================================================================
 # MIT License
@@ -46,14 +46,14 @@ def _writer(file_path, new_file, method, flag, **kwargs):
     This function facilitates reading from *file_path* and writing to
     *new_file* with the provided method by looping through each line
     of the file_path of fixed length, specified by *block_size*.
-    
+
     Note: Since ``cryptography`` module's AESGCM encryption appends a MAC
           after the encrypted part, hence the *nonce* is calculated after
           every block is read for encryption.
           The same is done for decryption, but the appended *nonce* is read
           along with the block data.
           The same *nonce* is never reused.
-    
+
     :param file_path: File to be written on.
     :param new_file: Name of the encrypted/decrypted file to written upon.
     :param method: The way in which the file must be overwritten.
@@ -103,6 +103,7 @@ def locker(file_path, password, remove=True, **kwargs):
     Encryption or decryption depends upon the file's extension.
     The user's encryption or decryption task is almost automated since
     *encryption* or *decryption* is determined by the file's extension.
+    
     :param file_path: File to be written on.
     :param password: Password to be used for encryption/decryption.
     :param remove: If set to True, the the file that is being
@@ -113,8 +114,11 @@ def locker(file_path, password, remove=True, **kwargs):
                 ext = extension to be appended to the files.
                 iterations = no. of iterations to derive the the key
                              from the password.
+                algo = The PBKDF2 hashing algorithm to use.
                 dklen = length of key after PBK derivation.
                 metadata = associated metadata written to file.
+                method = set method manually (`encrypt` or `decrypt`)
+                new_file = set new file path to be written upon.
     :return: None
     """
 
@@ -123,15 +127,33 @@ def locker(file_path, password, remove=True, **kwargs):
     iterations = kwargs.get('iterations', 50000)
     dklen = kwargs.get('dklen', 32)
     metadata = kwargs.get('metadata', b'Encrypted-using-Pycryptor')
+    algo = kwargs.get('algo', 'sha512')
+    _method = kwargs.get('method')
+    _new_file = kwargs.get('new_file')
+
+    # check for new-file's existence
+    if _new_file is not None:
+        if os.path.exists(_new_file):
+            if os.path.samefile(file_path, _new_file):
+                raise ValueError(f'Cannot process with the same file.')
+            os.remove(_new_file)
+
+    # check for method validity
+    if _method is not None:
+        if _method not in ['encrypt', 'decrypt']:
+            raise ValueError(f'Invalid method: `{_method}`. '
+                             'Method can be "encrypt" or "decrypt" only.')
+
+    # use auto-functionality or `_method`.
+    _auto_method = 'encrypt' if not file_path.endswith(ext) else 'decrypt'
+    method = _method or _auto_method
 
     # The file is being decrypted.
-    if file_path.endswith(ext):
-        method = 'decrypt'
+    if method == 'decrypt':
         flag = False
-        new_file = os.path.splitext(file_path)[0]
+        new_file = _new_file or os.path.splitext(file_path)[0]
 
         with open(file_path, 'rb') as file:
-
             # Check if file can be decrypted.
             check_metadata = file.read(len(metadata))
             if not metadata == check_metadata:
@@ -143,14 +165,13 @@ def locker(file_path, password, remove=True, **kwargs):
 
     # The file is being encrypted
     else:
-        method = 'encrypt'
         flag = True
-        new_file = file_path + ext
+        new_file = _new_file or file_path + ext
         salt = os.urandom(32)
 
     # Create a *password_hash* and *cipher* with
     # required method.
-    password_hash = hashlib.pbkdf2_hmac('sha512', password,
+    password_hash = hashlib.pbkdf2_hmac(algo, password,
                                         salt, iterations, dklen)
     cipher_obj = getattr(AESGCM(password_hash), method)
     crp = partial(cipher_obj, associated_data=metadata)
