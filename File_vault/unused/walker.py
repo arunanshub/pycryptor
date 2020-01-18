62c0c51134fb1906ec872bd4b08d31dfdd118173
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2020-01-18 12:14:03 +0000

    Made the code less verbose

--- File_vault/unused/walker.py
+++ File_vault/unused/walker.py
@@ -1,30 +1,21 @@
 import os
 
 
-# define an extension list here if you want to filter
-# out only selected extensions
-# extension_list = [...]
-
-def walker(file_path):
+def walker(path, exts=None):
     """
     This function recursively walks into a specified
     directory *file_path* and yields (i.e., creates generator
     object) which specifies the absolute path of the files
-    The commented out part is for filtering out files with only
-    targeted extensions.
+
+    `exts` is an iterable with all extensions to be included
+    in `walker`'s output. If `exts` is specified, then the 
+    files which ends with it are only yielded.
     """
-
-    path0 = os.path.normpath(file_path)
-    path = os.path.abspath(path0)
-
-    try:
-        for (root, dirs, files) in os.walk(path):
-            for file in files:
-                # extn = os.path.splitext(file)[1]
-                # if extn in extension_list
-
-                # Indent the two lines if you want to filter out
-                target = os.path.join(root, file)
-                yield target
-    except Exception:
-        pass
+    path = os.path.abspath(path)
+    for (root, _, files) in os.walk(path):
+        for each in files:
+            if exts is not None:
+                if os.path.splitext(each)[1] in exts:
+                    yield os.path.join(root, each)
+            else:
+                yield os.path.join(root, each)
